3ac0933169dab1484740996187fe6abb2c448b1e
Sam David <sfd1987@gmail.com>
2015-06-04 15:32:09 -0700

    thru step 10

--- simple-todos.js
+++ simple-todos.js
@@ -23,12 +23,7 @@ if (Meteor.isClient) {
     "submit .new-task": function (event) {
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),
-        owner: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call("addTask",text);
 
       event.target.text.value = "";
       // prevents default form submit
@@ -40,15 +35,34 @@ if (Meteor.isClient) {
   });
   Template.task.events({
     "click .toggle-checked": function() {
-      console.log(this)
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call("setChecked", this._id, ! this.checked)
     },
     "click .delete": function() {
-      console.log(this);
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTask: function(text) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: {checked: setChecked}});
+  }
+})
