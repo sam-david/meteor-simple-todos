b44f4e0f15dfc5e7f8dfd5aa5a46d86ddc19f2eb
Sam David <sfd1987@gmail.com>
2015-06-04 15:10:38 -0700

    completed thru step 8

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,17 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -21,6 +31,9 @@ if (Meteor.isClient) {
       event.target.text.value = "";
       // prevents default form submit
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
   Template.task.events({
